092bd52fc88d791ad31f8ba2d5db2481acff891a
zuojiayi <m18332360209@163.com>
2017-08-31 10:37:40 +0800

    左菜单右商品联动

--- README.md
+++ README.md
@@ -21,9 +21,9 @@ npm run build --report
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
 
-# css的预编译 stylus
-# 前6章 header组件中一像素边框 背景模糊属性 使用axios获取数据 props组件间数据传递  评分star组件  弹窗蒙层
-# 8/29/2017 第7章 goods组件  左右俩栏 底部的购物车功能
+### css的预编译 stylus
+### 前6章 header组件中一像素边框 背景模糊属性 使用axios获取数据 props组件间数据传递  评分star组件  弹窗蒙层
+# 第7章 goods组件  左右俩栏 底部的购物车功能 (8/29/2017)
 ## 7.1 
 内容会超出视口，可以向下滚动，但右侧没有滚动条
 classMap type的值对应的图标也可以抽象成一个组件，虽然只是一个span标签，可以对这个进行优化
@@ -31,5 +31,21 @@ classMap type的值对应的图标也可以抽象成一个组件，虽然只是�
 tips：display：table 不管一行还是多行都可以垂直居中
 ## 7.2
 左侧菜单布局  抽象icon组件
-## 7.3
+## 7.3 (8/30/2017)
+右侧食品布局
 点击左侧菜单，右侧内容联动
+## 7.4
+商品图片固定大小，右侧名称描述是自适应，用到flex布局
+## 7.6
+better-scroll
+rel特殊属性：vue中获取DOM
+## 7.7
+计算右侧每一部分的高度，计算滚动距离，当某一部分显示时，对应左侧的index值的menu设置高亮
+tips：设置class时，如：food-list-hook,(添加hook)不是为了设置样式，是为了js获取DOM使用
+## 7.8
+左侧高亮效果实现。点击左侧menu右侧商品显示对应的位置
+better-scroll监听了touchstart和touchend这些，prevent default 取消默认事件了，在this.menuScroll的第二个参数{}中设置click:true。
+存在问题：在移动端设置click：true后只派发一次click，但是在pc端，最开始的点击并没有给取消默认事件掉，所以pc端这里会触发2次click事件。 
+解决：在点击的时候调用方法传入$event，在better-scroll中派发的事件和原生的有一个区别，插件中有event._constructed值为true，而原生的没有。
+tips:1、通过ref="element",this.$refs.element 来获取原生DOM 2、在操作改变DOM后，使用this.$nextTick的回调来更新。
+## 7.9 (8/31/2017)
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="goods">
     <!--左侧菜单-->
-  	<div class="menu-wrapper">
+  	<div class="menu-wrapper" ref="menuWrapper">
   	  <ul >
-  	    <li v-for="item in goods" class="menu-item">
+  	    <li v-for="(item, index) in goods" class="menu-item" :class="{'current':currentIndex===index}" @click="selectMenu(index,$event)">
   	      <span class="text border-1px">
   	        <v-icon :item="item"></v-icon>
-  	        <!--<v-icon  v-show="item.type>0" class="icon" :class="classMap[item.type]"></v-icon>-->
-  	        <!--<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>-->
+  	        <!--抽象成组件之前：<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>-->
   	        {{item.name}}
   	      </span>
   	    </li>
   	  </ul>
   	</div> 
   	<!--右侧商品-->
-  	<div class="foods-wrapper">
-  	  
+  	<div class="foods-wrapper" ref="foodsWrapper">
+  	  <ul>
+  	  	<li v-for="item in goods" class="food-list food-list-hook">
+  	  	  <h1 class="title">{{item.name}}</h1>
+  	  	  <ul>
+  	  	  	<li v-for="food in item.foods" class="food-item border-1px">
+  	  	  	  <div class="icon">
+  	  	  	    <img width="57" height="57" :src="food.icon"/>
+  	  	  	  </div>
+  	  	  	  <div class="content">
+  	  	  	  	<h2 class="name">{{food.name}}</h2>
+  	  	  	  	<p class="desc">{{food.description}}</p>
+  	  	  	  	<div class="extra">
+  	  	  	  		<span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+  	  	  	  	</div>
+  	  	  	  	<div class="price">
+  	  	  	  		<span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+  	  	  	  		<!--不是所有商品都有降价的，需要判断-->
+  	  	  	  	</div>
+  	  	  	  </div>
+  	  	  	</li>
+  	  	  </ul>
+  	  	</li>
+  	  </ul>
   	</div>
     
   </div>
@@ -23,6 +44,8 @@
 
 <script type="text/ecmascript-6">
   import icon from '../icon/icon';
+  import BScroll from 'better-scroll';
+  
   const ERR_OK = 0;
   
 	export default {
@@ -33,8 +56,23 @@
 	  },
 	  data () {
 	    return {
-	      goods: [] // created中获取的数据
+	      goods: [], // created中获取的数据
+	      listHeight: [], // 商品区块距顶部的高度
+	      scrollY: 0
 	    };
+	  },
+	  computed: {
+	    currentIndex () {
+	      for (let i = 0; i < this.listHeight.length; i++) {
+	        let height1 = this.listHeight[i],
+	            height2 = this.listHeight[i + 1];
+	      	if (!height2 || (this.scrollY < height2 && this.scrollY >= height1)) {
+	      		// 如果已经在最后一个或者scrollY在两者之间时
+	      		return i;
+	      	}
+	      }
+	      return 0;
+	    }
 	  },
 	  created () {
 	    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -42,9 +80,48 @@
 	      response = response.data;
 	      if (response.errno === ERR_OK) { // 如果成功了
 	      	this.goods = response.data;
-	      	console.log(this.goods);
-	      };
+//	      	console.log(this.goods);
+          // 异步更新DOM，数据改变的时候，DOM并没有变化，这里scroll计算的高度就有问题
+          this.$nextTick(() => {
+            // 回调中更新DOM
+            this._initScroll();
+            this._calculateHeight();
+          });
+	      }
 	    });
+	  },
+	  methods: {
+	    selectMenu (index, event) {
+	      if (!event._constructed) {
+	      	return;
+	      }
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook'),
+            el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      },
+	    _initScroll () {
+	      // 第一个参数是dom，第二个参数是json
+	      this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+	        click: true  // 设置后可以点击，默认派发了一个点击事件
+	      });
+	      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+	        probeType: 3  // 实时监听位置
+	      });
+	      this.foodsScroll.on('scroll', (pos) => {
+	        this.scrollY = Math.abs(Math.round(pos.y)); // pos一个对象，里边包含xy的位置。小数
+	      });
+	    },
+	    _calculateHeight () {
+	      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook'),
+	          height = 0;
+	      this.listHeight.push(height);
+	      for (let i = 0; i < foodList.length; i++) {
+	      	// 遍历每一个区块的高度
+	      	height += foodList[i].clientHeight;
+	      	this.listHeight.push(height);
+	      }
+	      // console.log(this.listHeight);
+	    }
 	  },
 	  components: {
 	    'v-icon': icon
@@ -71,8 +148,16 @@
         width: 56px
         height: 54px
         line-height: 14px
-        margin: 0 auto
+        padding: 0 12px
         text-align: center
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight: 700
+          .text
+            border-none() /* 去掉下边框 */
         .text
           display: table-cell
           width: 56px
@@ -81,4 +166,50 @@
           font-size: 12px
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        padding: 18px
+        &:not(:last-of-type)
+          border-1px(rgba(7, 17, 27, 0.1))
+        .icon
+          flex: 0 0 57px
+          width: 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            line-height: 12px
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
 </style>
